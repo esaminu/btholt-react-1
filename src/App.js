524f4818c4879427b53d4d536b2eeae627b27533
esaminu <esaminu1@icloud.com>
2022-09-05 00:05:57 +0400

    feat: add pet details

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { render } from 'react-dom';
 
 import SearchParams from "./SearchParams";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 render(<App />, document.getElementById("root"));
