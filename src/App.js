7dc40abd8c447872df80bf0f2b9612b9628503e9
esaminu <esaminu1@icloud.com>
2022-10-10 00:31:35 +0400

    feat: add themes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { render } from 'react-dom';
+import { useState } from "react";
 
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
 const App = () => {
+  const theme = useState("darkblue");
+
   return (
-    <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext.Provider value={theme}>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   );
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+
+import ThemeContext from "./ThemeContext";
 
 import useBreedList from "./useBreedList";
 import Results from "./Results";
@@ -11,6 +13,8 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const res = await fetch(
@@ -81,7 +85,20 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
